e8cd3e1ba8f36621690eac7f772af9fbcf8aa43e
Alexander Wolf <74824365+awids-modules@users.noreply.github.com>
2021-04-24 01:11:48 +0200

    Update clickncollect.php

--- NEW_FILES/lang/german/extra/clickncollect.php
+++ NEW_FILES/lang/german/extra/clickncollect.php
@@ -1,3 +1,11 @@
 <?php
+######################################################################################
+# MODUL: Click&Collect (clickncollect)
+# VERSION: 1.0.2
+# RELEASE-DATE: 2021-04-25
+# AUTHOR: awids
+# PLATFORM: modified eCommerce Shopsoftware 2.0.x.x
+######################################################################################
+
 define('MODULE_SHIPPING_CLICKNCOLLECT_ERROR_DAY', 'Bitte geben Sie ein Abhol-Datum an.');
 define('MODULE_SHIPPING_CLICKNCOLLECT_ERROR_TIME', 'Bitte geben Sie eine Abhol-Zeit an.');
